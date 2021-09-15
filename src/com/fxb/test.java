3d1596a34b53c8857e9f431171677fed73bf5c50
FXB <2675667089@qq.com>
2021-09-15 18:49:29 +0800

    Initial commit

---
+++ src/com/fxb/test.java
@@ -0,0 +1,13 @@
+package com.fxb;
+/*
+ * @Time  :      2021/09/15 18:44
+ * @Author:      方小白
+ * @class_name:  test
+ * @Software  :  IntelliJ IDEA
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
